69480d4a7073f1dc0206565e43493596a8b4ee69
Jason <hwanth85@gmail.com>
2016-10-06 12:29:43 +0900

    user password encoder test 추가..

---
+++ src/test/java/kr/co/xfilegolf/UserPasswordEncoderTest.java
@@ -0,0 +1,29 @@
+package kr.co.xfilegolf;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * @author jason, Moon
+ * @since 2016-10-05
+ */
+public class UserPasswordEncoderTest {
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Before
+    public void setUp() throws Exception {
+        bCryptPasswordEncoder = new BCryptPasswordEncoder();
+    }
+
+    @Test
+    public void passWordHashTest() throws Exception {
+
+        String password = "1234";
+
+        String passwordHash = bCryptPasswordEncoder.encode(password);
+
+        System.out.println("passwordHash = " + passwordHash);
+    }
+}
