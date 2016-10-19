75333a4f4a99cb2724bdbc050e6a7c9d93f663c4
Jason <hwanth85@gmail.com>
2016-10-19 22:39:03 +0900

    리팩토링.

--- src/main/java/kr/co/xfilegolf/user/UserController.java
+++ src/main/java/kr/co/xfilegolf/user/UserController.java
@@ -119,51 +119,7 @@ public class UserController {
             }
         }
 
-        // ROLE_USER Error Check
-        if (userForm.getRole().equals("ROLE_USER")) {
-
-            if (StringUtils.isEmpty(userForm.getAddress())) {
-
-                FieldError fieldError = new FieldError("userForm", "address", "주소는 반드시 입력해야 합니다.");
-
-                result.addError(fieldError);
-            }
-        } else {
-            if (StringUtils.isEmpty(userForm.getPresidentName())) {
-
-                FieldError fieldError = new FieldError("userForm", "presidentName", "대표자명은 반드시 입력해야합니다.");
-
-                result.addError(fieldError);
-            }
-
-            if (StringUtils.isEmpty(userForm.getAgencyName())) {
-
-                FieldError fieldError = new FieldError("userForm", "agencyName", "대리점명은 반드시 입력해야합니다.");
-
-                result.addError(fieldError);
-            }
-
-            if (StringUtils.isEmpty(userForm.getAgencyAddress())) {
-
-                FieldError fieldError = new FieldError("userForm", "agencyAddress", "대리점 주소는 반드시 입력해야합니다.");
-
-                result.addError(fieldError);
-            }
-
-            if (StringUtils.isEmpty(userForm.getBusinessNumber())) {
-
-                FieldError fieldError = new FieldError("userForm", "businessNumber", "사업자등록번호는 반드시 입력해야합니다.");
-
-                result.addError(fieldError);
-            }
-
-            if (StringUtils.isEmpty(userForm.getPhoneNumber())) {
-
-                FieldError fieldError = new FieldError("userForm", "phoneNumber", "대표전화는 반드시 입력해야합니다.");
-
-                result.addError(fieldError);
-            }
-        }
+        result = validation(userForm, result);
 
         if (!SecurityUtils.hasAdminRole() && result.hasErrors()) {
             return "/user/user-register";
@@ -236,5 +192,56 @@ public class UserController {
         userModifyForm.setAgencyAddress(user.getAgencyAddress());
         userModifyForm.setBusinessNumber(user.getBusinessNumber());
         userModifyForm.setPhoneNumber(user.getPhoneNumber());
+    }
+
+    private BindingResult validation(@Valid UserForm userForm, BindingResult result) {
+
+        // ROLE_USER Error Check
+        if (userForm.getRole().equals("ROLE_USER")) {
+
+            if (StringUtils.isEmpty(userForm.getAddress())) {
+
+                FieldError fieldError = new FieldError("userForm", "address", "주소는 반드시 입력해야 합니다.");
+
+                result.addError(fieldError);
+            }
+        } else {
+            if (StringUtils.isEmpty(userForm.getPresidentName())) {
+
+                FieldError fieldError = new FieldError("userForm", "presidentName", "대표자명은 반드시 입력해야합니다.");
+
+                result.addError(fieldError);
+            }
+
+            if (StringUtils.isEmpty(userForm.getAgencyName())) {
+
+                FieldError fieldError = new FieldError("userForm", "agencyName", "대리점명은 반드시 입력해야합니다.");
+
+                result.addError(fieldError);
+            }
+
+            if (StringUtils.isEmpty(userForm.getAgencyAddress())) {
+
+                FieldError fieldError = new FieldError("userForm", "agencyAddress", "대리점 주소는 반드시 입력해야합니다.");
+
+                result.addError(fieldError);
+            }
+
+            if (StringUtils.isEmpty(userForm.getBusinessNumber())) {
+
+                FieldError fieldError = new FieldError("userForm", "businessNumber", "사업자등록번호는 반드시 입력해야합니다.");
+
+                result.addError(fieldError);
+            }
+
+            if (StringUtils.isEmpty(userForm.getPhoneNumber())) {
+
+                FieldError fieldError = new FieldError("userForm", "phoneNumber", "대표전화는 반드시 입력해야합니다.");
+
+                result.addError(fieldError);
+            }
+        }
+
+        return result;
     }
 }
